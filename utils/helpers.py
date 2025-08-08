37861424678ee4eb13e9472898ce84f470733f58
411A <01AliAbdi@gmail.com>
2025-08-08 17:26:01 +0330

    🧑‍💻 Better utils naming

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,13 +1,13 @@
 """Utility functions for the application."""
+
 import re
-import asyncio
 import base64
 import hashlib
 import random
 import string
 import time
 from configs.settings import AVAILABLE_LANGUAGES_LITERAL, AVAILABLE_LANGUAGES_LIST
-from typing import Dict, Any, Optional
+
 
 def extract_bot_token(text: str) -> str:
     """Extract a valid bot token from text using regex.
@@ -24,11 +24,13 @@ def extract_bot_token(text: str) -> str:
     match = re.search(pattern, text)
     return match.group(0) if match else ''
 
+
 def shorten_token(token: str) -> str:
     """Return the token in a shortened format: first 3 chars, '…', last 3 chars."""
     if len(token) <= 6:
         return token
     return f"{token[:3]}…{token[-3:]}"
+
 
 def check_language_availability(language_code: str) -> AVAILABLE_LANGUAGES_LITERAL:
     """Check if a language code is available in the supported languages list.
@@ -41,6 +43,7 @@ def check_language_availability(language_code: str) -> AVAILABLE_LANGUAGES_LITER
     """
     global AVAILABLE_LANGUAGES_LIST
     return language_code if language_code in AVAILABLE_LANGUAGES_LIST else 'en'
+
 
 def generate_anonymous_id(user_id: int, user_fname: str = None, with_history: bool = False) -> str:
     '''Generate a unique, hashtag-friendly anonymous ID.'''
@@ -64,34 +67,3 @@ def generate_anonymous_id(user_id: int, user_fname: str = None, with_history: bo
     if with_history:
         return f"#{anon_id}"
     return f"{anon_id}"
-
-# Async-Safe Singleton Cache for Admins Repliesclass
-class AdminsReplyCache:
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(AdminsReplyCache, cls).__new__(cls)
-        return cls._instance
-
-    def __init__(self):
-        # Initialize only once
-        if not hasattr(self, '_cache'):
-            self._cache: Dict[int, Dict[str, Any]] = dict()
-            self._lock = asyncio.Lock()
-
-    async def set(self, admin_id: int, state: dict) -> None:
-        async with self._lock:
-            self._cache[admin_id] = state
-
-    async def get(self, admin_id: int) -> Optional[Dict[str, Any]]:
-        async with self._lock:
-            return self._cache.get(admin_id)
-
-    async def remove(self, admin_id: int) -> Optional[Dict[str, Any]]:
-        async with self._lock:
-            return self._cache.pop(admin_id, None)
-
-    async def exists(self, admin_id: int) -> bool:
-        async with self._lock:
-            return admin_id in self._cache
